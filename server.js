8b2a86200ea4e139d3fefe66c4365025476b0b83
Baterik <erik_bonou2@hotmail.com>
2024-03-05 21:58:07 -0600

    Se importa carRoutes, se utiliza .use(json) para permitir leer json en express

--- server.js
+++ server.js
@@ -4,11 +4,21 @@
 //*3. Usar el app.listen para abrir puertos
 import {connect} from './config.js';
 import express from 'express';
+import carRoutes from './routes/carRoutes.js';
 
 connect();
 
 const api = express();
 
+api.use(express.json());
+
 api.listen(8000, ()=>{
     console.log('API corriendo en el puerto 8000');
 });
+
+//*string(nombre de la ruta)
+api.get('/test', (req, res) => {
+    res.send('Hola, la prueba funciona');
+})
+
+api.use(carRoutes);
