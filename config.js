301187abf027fab385b78363b221427bcec6f59a
Baterik <erik_bonou2@hotmail.com>
2024-03-05 21:51:33 -0600

    se crea una constante para usar mongoose y la funcion once

--- config.js
+++ config.js
@@ -5,7 +5,18 @@ dotenv.config();
 
 
 const connect = ()=> {
-    mongoose.connect(process.env.DB_URI)
+    mongoose.connect(process.env.DB_URI);
+
+    const connection = mongoose.connection;
+
+    connection.once('open', () => {
+        console.log('Conexion exitosa a la base de datos');
+
+    });
+
+    connection.once('error', ()=>{
+        console.error('Error al conectarse a la DB')
+    })
 };
 
 export {connect};
