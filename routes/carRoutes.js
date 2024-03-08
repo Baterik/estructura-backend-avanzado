1fc7916a97d8ead3c359e26b31b9e657aad65a5b
Baterik <erik_bonou2@hotmail.com>
2024-03-07 21:01:30 -0600

    Se crean las rutas del CRUD

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createCar } from "../controllers/carController.js";
+import { createCar, deleteCar, getAllCars, getCarById, updateCar } from "../controllers/carController.js";
 
 const carRoutes = express.Router();
 
 carRoutes.post('/cars', createCar);
+carRoutes.get('/cars', getAllCars);
+carRoutes.get('/cars/:carId', getCarById);
+carRoutes.put('/cars/:carId', updateCar);
+carRoutes.delete('/cars/:carId', deleteCar );
 
 export default carRoutes
