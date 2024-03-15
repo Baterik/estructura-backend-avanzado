e1ea87a922d49d549d8cddf6bc4fc3ddac212468
Baterik <erik_bonou2@hotmail.com>
2024-03-14 22:13:04 -0600

    se crea la ruta para consultar todos los libros y busqueda por Id

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createBook} from '../controllers/bookController.js';
+import {createBook, getBookById, getAllBooks} from '../controllers/bookController.js';
 
 
 const bookRoutes = express.Router();
@@ -8,5 +8,9 @@ const bookRoutes = express.Router();
 bookRoutes.post('/', createBook);
 
 //*ToDo: get book by id
+bookRoutes.get('/:bookId', getBookById);
+
+//ToDo get all books
+bookRoutes.get('/', getAllBooks)
 
 export default bookRoutes;
