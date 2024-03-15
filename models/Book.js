d88ccbbe8db111ed930abc1972301ed49b4c3d65
Baterik <erik_bonou2@hotmail.com>
2024-03-14 22:13:55 -0600

    Se modifica el array de autors a un objectId

--- models/Book.js
+++ models/Book.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
-import { authorSchema } from "./Author.js";
+// import { authorSchema } from "./Author.js";
 
 const bookSchema = new mongoose.Schema({
     title: String,
     year: Number,
     genre: String,
     isbn: String,
-    authors: [authorSchema],
+    authors: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author',
+    }],
 });
 
 export default mongoose.model('Book', bookSchema);
